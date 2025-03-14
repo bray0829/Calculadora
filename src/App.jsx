fb1a871fbbc25ed09d79f9634a133c4ab06316d9
bray0829 <130944215+bray0829@users.noreply.github.com>
2025-03-14 18:01:21 +0000

    cmabi un 1 por 2

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         <input type="text" value={input} readOnly />
       </div>
       <div className="buttons">
-        <button onClick={() => handleClick("1")}>1</button>
+        <button onClick={() => handleClick("1")}>2</button>
         <button onClick={() => handleClick("2")}>2</button>
         <button onClick={() => handleClick("3")}>3</button>
         <button onClick={() => handleClick("+")}>+</button>
